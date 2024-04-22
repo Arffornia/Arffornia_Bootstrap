a4536d8ae85c19f20bc808679f4d9b48dcd12b76
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-04-22 08:06:41 +0200

    ✅ Add guidelines

---
+++ src/main/java/Bootstrap.java
@@ -0,0 +1,13 @@
+public class Bootstrap {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        /*
+            TODO
+
+            Check & download Java 17
+            Check & download Arffornia launcher (last version)
+            Launch Arffornia launcher with java 17 version
+         */
+    }
+}
